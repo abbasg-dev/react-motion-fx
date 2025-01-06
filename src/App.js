ae34cac07357fea3db50859fa06a57df290dfe54
aghaith <abbasghayth5@gmail.com>
2025-01-06 21:03:42 +0400

    Create the content section and style it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import HeroText from "./components/HeroText";
 import ImageSection from "./components/ImageSection";
+import ContentSection from "./components/ContentSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <HeroText />
       <ImageSection />
+      <ContentSection />
     </>
   );
 }
